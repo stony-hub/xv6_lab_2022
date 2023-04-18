2d0fe946f7abb59b1f894326201e46c50b9b3e8c
Jinsong Huang <huangjs@pku.edu.cn>
2023-04-18 18:12:17 +0800

    finish ph

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t lock[NBUCKET];
 
 
 double
@@ -41,6 +42,7 @@ void put(int key, int value)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&lock[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -54,6 +56,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&lock[i]);
 
 }
 
@@ -62,11 +65,12 @@ get(int key)
 {
   int i = key % NBUCKET;
 
-
+  pthread_mutex_lock(&lock[i]);
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
+  pthread_mutex_unlock(&lock[i]);
 
   return e;
 }
@@ -105,6 +109,9 @@ main(int argc, char *argv[])
   void *value;
   double t1, t0;
 
+  for (int i=0; i<NBUCKET; i++) {
+    pthread_mutex_init(&lock[i], NULL);
+  }
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
